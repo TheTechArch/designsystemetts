0ae57fe908572535a1095d7a29dc30c26fa671bb
Rune T. Larsen <rune@hjemmekino.no>
2025-03-16 09:31:54 +0100

    First checkin to show problem

---
+++ src/designsystemetts/designsystemetts/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import '@digdir/designsystemet-css';
+import '@digdir/designsystemet-theme'; // or custom theme CSS file
+
+import { Button } from '@digdir/designsystemet-react';
+<Button variant='secondary'>I am a button!</Button>;
+
+function App() {
+  return (
+    <>
+          <Button variant='secondary'>I am a button!</Button>;
+    </>
+  )
+}
+
+export default App
