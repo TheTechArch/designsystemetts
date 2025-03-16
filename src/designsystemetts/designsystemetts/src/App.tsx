f7d8c0ca269b790fd0b494f8f8a02dc677919125
Rune T. Larsen <rune@hjemmekino.no>
2025-03-16 09:39:22 +0100

    dobule truble

--- src/designsystemetts/designsystemetts/src/App.tsx
+++ src/designsystemetts/designsystemetts/src/App.tsx
@@ -3,7 +3,6 @@ import '@digdir/designsystemet-css';
 import '@digdir/designsystemet-theme'; // or custom theme CSS file
 
 import { Button } from '@digdir/designsystemet-react';
-<Button variant='secondary'>I am a button!</Button>;
 
 function App() {
   return (
