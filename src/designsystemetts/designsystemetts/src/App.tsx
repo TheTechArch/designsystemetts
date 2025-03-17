6a57aebcc63fcd06516156c0f04a4f03ff44d903
Michael Marszalek <mimarz@gmail.com>
2025-03-17 08:14:28 +0100

    fix: css type error and intellisens for data color and size

--- src/designsystemetts/designsystemetts/src/App.tsx
+++ src/designsystemetts/designsystemetts/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import '@digdir/designsystemet-css';
+import '@digdir/designsystemet-css/index.css';
 import '@digdir/designsystemet-theme'; // or custom theme CSS file
 
 import { Button } from '@digdir/designsystemet-react';
@@ -7,7 +7,7 @@ import { Button } from '@digdir/designsystemet-react';
 function App() {
   return (
     <>
-          <Button variant='secondary'>I am a button!</Button>;
+          <Button variant='secondary'>I am a button!</Button>
     </>
   )
 }
